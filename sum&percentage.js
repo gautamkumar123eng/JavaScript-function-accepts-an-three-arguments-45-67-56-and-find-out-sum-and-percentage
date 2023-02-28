98286b4d5928e2decfb90d2627416b25839afdc7
Gautam Kumar <91307706+gautamkumar123eng@users.noreply.github.com>
2023-03-01 00:04:15 +0530

    Add files via upload

---
+++ sum&percentage.js
@@ -0,0 +1,21 @@
+function sum(a,b,c) {
+    let total = a+b+c ;
+    let ave = total/3;
+    console.log(ave);
+    console.log(total);
+    
+}
+sum(10,15,20);
+
+let p1 = prompt("enter value 1");
+let p2 = prompt("enter value 2");
+let p3 = prompt("enter value 3");
+
+let sum1 = (a,b,c) => {
+    let total = a+b+c ;
+    let ave = total/;
+    console.log(ave);
+    console.log(total);
+
+}
+sum1(10,15,20);
